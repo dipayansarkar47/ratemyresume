7e827533f07e1581338438c9f8040ebf5b5dc32d
Ashutosh Mukhopadhyay <debamoymukhopadhyay@Debamoys-MacBook-Pro.local>
2023-07-01 21:29:58 +0530

    sc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-500">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
